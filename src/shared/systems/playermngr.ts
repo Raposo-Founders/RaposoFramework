165b766f616af8a7e395064b198ab775a648964e
coolergate <coolergate@gmail.com>
2025-09-15 12:01:54 -0300

    Add "team" command

--- src/shared/systems/playermngr.ts
+++ src/shared/systems/playermngr.ts
@@ -1,17 +1,39 @@
 import { Players, RunService } from "@rbxts/services";
+import { registerConsoleFunction } from "shared/cmd/cvar";
+import conch from "shared/conch_pkg";
+import { Type, VarargType } from "shared/conch_pkg/conch";
 import { defaultEnvironments } from "shared/defaultinsts";
+import PlayerEntity, { PlayerTeam } from "shared/entities/PlayerEntity";
+import { gameValues } from "shared/gamevalues";
+import { finishDirectMessage, startDirectMessage } from "shared/network";
 import { getPlayermodelFromEntity } from "shared/playermodel";
 import ServerInstance from "shared/serverinst";
+import { BufferReader } from "shared/util/bufferreader";
+import { writeBufferString, writeBufferU8 } from "shared/util/bufferwriter";
 import { getLocalPlayerEntity } from "shared/util/localent";
 import { DoesInstanceExist } from "shared/util/utilfuncs";
 
 // # Constants & variables
+const TARGET_GROUP = 7203437 as const;
+const ADMIN_ROLES: string[] = [
+  "HOLDER",
+  "PRESIDENT",
+  "DIRECTOR",
+  "COMMANDER",
+  "WARDEN",
+  "DEVELOPER",
+  "CAPTAIN",
+  "SERGEANT",
+] as const;
 
 // # Functions
 
 // # Execution
 ServerInstance.serverCreated.Connect(inst => {
   inst.playerJoined.Connect((user, referenceId) => {
+    user.SetAttribute(gameValues.adminattr, ADMIN_ROLES.includes(user.GetRoleInGroup(TARGET_GROUP)));
+    user.SetAttribute(gameValues.modattr, user.GetAttribute(gameValues.adminattr));
+
     inst.entity.createEntity("SwordPlayerEntity", `PlayerEnt_${user.UserId}`, referenceId, user.UserId).andThen(ent => {
       print(`Player entity created for user ${user.Name} with ID ${ent.id}.`);
 
@@ -20,6 +42,67 @@ ServerInstance.serverCreated.Connect(inst => {
       ent.Spawn(new CFrame(0, 100, 0));
     });
   });
+
+  inst.network.listenPacket("team", (sender, bfr) => {
+    if (!sender || !sender.GetAttribute(gameValues.modattr)) return;
+
+    const reader = BufferReader(bfr);
+    const entityId = reader.string();
+    const team = reader.u8();
+
+    const targetEntity = inst.entity.entities.get(entityId);
+    if (!targetEntity || !targetEntity.IsA("PlayerEntity")) {
+      startDirectMessage(gameValues.cmdnetinfo, sender);
+      writeBufferString(`Invalid player entity ${entityId}`);
+      finishDirectMessage();
+
+      return;
+    }
+
+    // Check to see if the sender is just someone with tempmod
+    if (!sender.GetAttribute(gameValues.adminattr) && sender.GetAttribute(gameValues.modattr)) {
+
+      // Block the action if the target player is from the defenders team
+      if (targetEntity.team === PlayerTeam.Defenders) {
+        startDirectMessage(gameValues.cmdnetinfo, sender);
+        // writeBufferString("Moderators can't switch players off the Defenders team."); // Formal?
+        writeBufferString("Moderators can't mess with the Defenders' team."); // :)
+        finishDirectMessage();
+
+        return;
+      }
+    }
+
+    targetEntity.team = team;
+    targetEntity.Spawn();
+
+    startDirectMessage(gameValues.cmdnetinfo, sender);
+    writeBufferString(`Changed ${targetEntity.GetUserFromController()}'s team to ${PlayerTeam[team]}.`);
+    finishDirectMessage();
+  });
+});
+
+registerConsoleFunction(["team"], [conch.args.player(), conch.args.string()], "Changes a player's team")((ctx, user, team) => {
+  let targetEntity: PlayerEntity | undefined;
+  for (const ent of defaultEnvironments.entity.getEntitiesThatIsA("PlayerEntity")) {
+    if (ent.GetUserFromController() !== user) continue;
+    targetEntity = ent;
+    break;
+  }
+  assert(targetEntity, "Player has no entity.");
+
+  let targetTeamId = PlayerTeam.Spectators;
+
+  if (("defenders").match((team as string).lower())[0]) targetTeamId = PlayerTeam.Defenders;
+  if (("raiders").match((team as string).lower())[0]) targetTeamId = PlayerTeam.Raiders;
+  if (("spectators").match((team as string).lower())[0]) targetTeamId = PlayerTeam.Spectators;
+
+  defaultEnvironments.network.startWritingMessage("team");
+  writeBufferString(targetEntity.id);
+  writeBufferU8(targetTeamId);
+  defaultEnvironments.network.finishWritingMessage();
+
+  ctx.Reply("Finished executing.");
 });
 
 if (RunService.IsClient()) {
