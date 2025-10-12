38efab8e56b6237cd3385059264a29f5d8a23585
coolergate <coolergate@gmail.com>
2025-10-12 13:38:14 -0300

    Equipping & unequipping swords

--- src/systems/UserInput.ts
+++ src/systems/UserInput.ts
@@ -3,19 +3,31 @@ import { defaultEnvironments } from "defaultinsts";
 import { getLocalPlayerEntity } from "./localent";
 
 // # Variables
-let autoAttackEnabled = UserInputService.TouchEnabled;
+let autoAttackEnabled = false;
 
 // # Functions
-export function setAutoAttack(state: Enum.UserInputState, enabled: boolean) {
+function setAutoAttack(state: Enum.UserInputState, enabled: boolean) {
   if (state.Name !== "End") return;
   autoAttackEnabled = enabled;
 }
 
-export function swordsAttack() {
+function swordsAttack() {
   const entity = getLocalPlayerEntity();
   if (!entity?.IsA("SwordPlayerEntity") || entity.health <= 0) return;
 
   entity.Attack1();
+}
+
+function swordsEquipToggle(state: Enum.UserInputState) {
+  if (state.Name !== "End") return;
+
+  const entity = getLocalPlayerEntity();
+  if (!entity?.IsA("SwordPlayerEntity") || entity.health <= 0) return;
+
+  if (entity.IsWeaponEquipped())
+    entity.Unequip();
+  else
+    entity.Equip();
 }
 
 function stylizeButton(actionName: string, anchorPoint: Vector2, position: UDim2, size: UDim2) {
@@ -44,14 +56,23 @@ if (RunService.IsClient()) {
 
 // Setup mobile inputs
 if (RunService.IsClient()) {
+  ContextActionService.BindAction("swords_equipToggle", (_, state) => swordsEquipToggle(state), true, Enum.KeyCode.One);
   ContextActionService.BindAction("swords_autoAttack", (_, state) => setAutoAttack(state, !autoAttackEnabled), true, Enum.KeyCode.R);
 
+  ContextActionService.SetTitle("swords_equipToggle", "Equip");
   ContextActionService.SetTitle("swords_autoAttack", "AC");
+
+  stylizeButton(
+    "swords_equipToggle",
+    new Vector2(0, 0.5),
+    new UDim2(0, 0, 0.5, 0),
+    UDim2.fromScale(0.3, 0.3),
+  );
 
   stylizeButton(
     "swords_autoAttack",
     new Vector2(0, 1),
-    new UDim2(0, 0, 1, -16),
+    new UDim2(0, 0, 1, 0),
     UDim2.fromScale(0.25, 0.25),
   );
 }
