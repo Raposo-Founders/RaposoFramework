0293503b8ef6c3ede086e4e0be5ff7e9f4eb91e0
coolergate <coolergate@gmail.com>
2025-10-12 11:58:49 -0300

    Make fairzone display smaller for mobile devices

--- src/UI/hud/fairzonetopdisplay.tsx
+++ src/UI/hud/fairzonetopdisplay.tsx
@@ -1,7 +1,16 @@
 import React from "@rbxts/react";
+import { UserInputService } from "@rbxts/services";
 import { uiValues } from "UI/values";
 
 export function FairzoneTopDisplay(props: React.PropsWithChildren) {
+  const scaleReference = React.createRef<UIScale>();
+
+  UserInputService.InputBegan.Connect(() => {
+    if (!scaleReference.current) return;
+
+    scaleReference.current.Scale = UserInputService.TouchEnabled ? 0.75 : 1;
+  });
+
   return (
     <frame
       AnchorPoint={new Vector2(0.5, 0)}
@@ -16,6 +25,7 @@ export function FairzoneTopDisplay(props: React.PropsWithChildren) {
         HorizontalAlignment={"Center"}
         SortOrder={"LayoutOrder"}
       />
+      <uiscale ref={scaleReference} />
       {props.children}
     </frame>
   );
