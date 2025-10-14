2ececebb47639b70511edad85cf813d36ca16c12
Guilherme Lemos <coolergate@gmail.com>
2025-10-14 15:24:59 -0300

    Add joke commands

---
+++ src/cmd/commands/jokecmds.ts
@@ -0,0 +1,29 @@
+import { ConsoleFunctionCallback } from "cmd/cvar";
+import { defaultEnvironments } from "defaultinsts";
+import { gameValues } from "gamevalues";
+import ServerInstance from "serverinst";
+import { sendSystemChatMessage } from "systems/ChatSystem";
+import { BufferReader } from "util/bufferreader";
+import { startBufferCreation, writeBufferString } from "util/bufferwriter";
+
+// # Constants & variables
+const CMD_INDEX_NAME = "cmd_joke";
+
+// # Bindings & execution
+
+ServerInstance.serverCreated.Connect(inst => {
+  inst.network.listenPacket(CMD_INDEX_NAME, info => {
+    if (!info.sender || !info.sender.GetAttribute(gameValues.modattr)) return;
+
+    const reader = BufferReader(info.content);
+
+    sendSystemChatMessage(`Nice try ${info.sender.Name}, but this is not Kohl's admin.`);
+  });
+}); 
+
+new ConsoleFunctionCallback(["fly", "ff", "forcefield", "invisible", "invis"], [])
+  .setCallback((ctx) => {
+    startBufferCreation();
+    writeBufferString("joke");
+    defaultEnvironments.network.sendPacket(CMD_INDEX_NAME);
+  });
